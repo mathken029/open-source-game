302507adda32d70ae009964d0f55f4411b9f9ef8
@_mathken <11295237+mathken029@users.noreply.github.com>
2023-09-19 19:28:40 +0900

    GUIが日本語で表示されるようになりました

--- Assets/first-person-action-template/Scripts/DisplayController.cs
+++ Assets/first-person-action-template/Scripts/DisplayController.cs
@@ -1,17 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DisplayController : MonoBehaviour
 {
+    //ポイント表示
+    [SerializeField] private TextMeshProUGUI pointText;
+    
+    //ポイント表示
+    [SerializeField] private TextMeshProUGUI explanationText;
+
     // ゲームのポイント（別クラスに分離する）
     private int point = 0;
-    
+
+    private void Start()
+    {
+        explanationText.text = "操作説明\n" +
+                               "マウス移動：武器を動かす";
+    }
+
     public void AddPoint(int amount)
     {
         point += amount;
     }
-		
+
+    private void Update()
+    {
+        pointText.text = "得点：" + point + "点";
+        
+    }
+
     void OnGUI ()
     {
         GUI.Box (new Rect (Screen.width - 260, 10, 100, 30), "得点：" + point + "点");
